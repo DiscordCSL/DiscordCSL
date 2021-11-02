753ef0eb9c37eeb856819d673625b8e16e53b179
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2021-11-02 11:19:37 -0400

    Update csl.plugin.js

--- csl.plugin.js
+++ csl.plugin.js
@@ -1,49 +1,64 @@
 //META{"name":"cslp"}*//
 
 class cslp {
-  // Constructor
-  constructor() {
-      this.initialized = false;
-  }
+    // Constructor
+    constructor() {
+        this.initialized = false;
+    }
 
-  // Meta
-  getName() { return "Custom Server Logos Patch"; }
-  getShortName() { return "CSL Patch"; }
-  getDescription() { return "A patch that adds 'data-guild-id' and allows Custom Server Logos to work as intended"; }
-  getVersion() { return "1.0.0"; }
-  getAuthor() { return "KayoticCarnige"; }
-  
-  // Load/Unload
-  load() { }
+    // Meta
+    getName() { return "Custom Server Logos Patch"; }
+    getShortName() { return "CSL Patch"; }
+    getDescription() { return "A patch that adds 'data-guild-id' and allows Custom Server Logos to work as intended"; }
+    getVersion() { return "2.0.0"; }
+    getAuthor() { return "KayoticCarnige"; }
 
-  unload() { }
+    // Load/Unload
+    load() { }
 
-  // Events
+    unload() { }
 
-  onMessage() {
-      // Called when a message is received
-  };
+    // Events
 
-  onSwitch() {
-      // Called when a server or channel is switched
-      document.getElementsByClassName("header-2V-4Sw")[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
-  };
+    onMessage() {
+        // Called when a message is received
+    };
 
-  observer(e) {
-      // raw MutationObserver event for each mutation
-  };
-  
-  // Start/Stop
-  start() {}
-     
-  stop() {
-      PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has stopped.");
-  };
+    onSwitch() {
+        // Called when a server or channel is switched
+    };
 
-  //  Initialize
-  initialize() {
-      this.initialized = true;
-      PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has started.");
+    observer(e) {
+        // raw MutationObserver event for each mutation
+    };
 
-  }
+    // Start/Stop
+    start() {
+        var oldURL = "";
+        var currentURL = window.location.href;
+        function checkURLchange(currentURL) {
+            if (currentURL != oldURL) {
+                oldURL = currentURL;
+            }
+
+            oldURL = window.location.href;
+            setTimeout(function () {
+                checkURLchange(window.location.href);
+                document.getElementsByClassName("container-3w7J-x")[0].setAttribute('data-guild-id', window.location.pathname.split('/')[2]);
+            }, 1);
+        }
+
+        checkURLchange();
+    }
+
+    stop() {
+        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has stopped.");
+    };
+
+    //  Initialize
+    initialize() {
+        this.initialized = true;
+        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has started.");
+
+    }
 }
