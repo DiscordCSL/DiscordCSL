738b5505b9d097976e8f3cec72e13af8b4e65b07
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2021-11-11 17:32:05 -0500

    Update cslp.js

--- cumcord/cslp.js
+++ cumcord/cslp.js
@@ -6,7 +6,6 @@ export default () => ({
 });
 
 
-/// From StackOverflow: https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
 var oldURL = "";
 var currentURL = window.location.href;
 function checkURLchange(currentURL) {
